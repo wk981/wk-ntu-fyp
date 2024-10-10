957db7aa1d5f07112ca77ee2126f598dbeca60c3
wk981 <jamesim445@gmail.com>
2024-10-11 00:36:23 +0800

    chore: init files for frontend and backend

---
+++ wgtpivotlo-backend/src/main/java/com/wgtpivotlo/wgtpivotlo/WgtPivotLoApplication.java
@@ -0,0 +1,13 @@
+package com.wgtpivotlo.wgtpivotlo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgtPivotLoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgtPivotLoApplication.class, args);
+	}
+
+}
---
+++ wgtpivotlo-backend/src/test/java/com/wgtpivotlo/wgtpivotlo/WgtPivotLoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgtpivotlo.wgtpivotlo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WgtPivotLoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

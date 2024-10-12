e5b06fe8cd7cc015958776192d6499d478993b79
wk981 <jamesim445@gmail.com>
2024-10-13 01:37:38 +0800

    chore: add sql schema sql and spring security init

---
+++ wgtpivotlo-backend/src/main/java/com/wgtpivotlo/wgtpivotlo/security/SecurityConfiguration.java
@@ -0,0 +1,19 @@
+package com.wgtpivotlo.wgtpivotlo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize.requestMatchers(HttpMethod.GET, "/**").permitAll().anyRequest().authenticated());
+        return http.build();
+    }
+}

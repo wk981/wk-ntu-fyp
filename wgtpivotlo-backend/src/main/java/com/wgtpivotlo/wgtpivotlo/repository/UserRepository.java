db35a0c420ee7792710a2414ebb25f59a42190e2
wk981 <jamesim445@gmail.com>
2024-10-13 02:56:26 +0800

    chore: spring data repo init

---
+++ wgtpivotlo-backend/src/main/java/com/wgtpivotlo/wgtpivotlo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.wgtpivotlo.wgtpivotlo.repository;
+
+import com.wgtpivotlo.wgtpivotlo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}

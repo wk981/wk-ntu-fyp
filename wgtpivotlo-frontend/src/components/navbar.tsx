ce7eedd075156fd4a8df28e256a34dcbdbf2db73
wk981 <jamesim445@gmail.com>
2025-01-14 01:26:09 +0800

    feat: implement dropdown navbar item

--- wgtpivotlo-frontend/src/components/navbar.tsx
+++ wgtpivotlo-frontend/src/components/navbar.tsx
@@ -3,9 +3,15 @@ import { BlueAnchorLink } from '@/components/blue-anchor-link';
 import { useAuth } from '@/features/auth/hook/useAuth';
 import { Button } from './ui/button';
 import { Profile } from './profile';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { NavItemDropdown } from './navbar-dropdown';
 
 export const Navbar = () => {
+  const exploreMoreItems = [
+    { label: 'Career', href: '/career' },
+    { label: 'Learning Timeline', href: '/timeline' },
+  ];
+
   const { isLoggedIn } = useAuth();
   const navigate = useNavigate();
   const onClick = async () => {
@@ -17,10 +23,12 @@ export const Navbar = () => {
   };
   return (
     <nav className="flex h-[60px] w-full bg-white text-lg gap-2 drop-shadow-xl items-center justify-between px-3">
-      <div className="flex gap-6">
-        <img src={HomeIcon}></img>
+      <div className="flex gap-6 items-center">
+        <Link to={'/'}>
+          <img src={HomeIcon}></img>
+        </Link>
         <BlueAnchorLink toPath={'questionaire'} name="questionaire" />
-        <BlueAnchorLink toPath={'expore'} name="explore" />
+        <NavItemDropdown label="Explore" items={exploreMoreItems} />
         <BlueAnchorLink toPath={'resume'} name="resume" />
       </div>
       {isLoggedIn ? (

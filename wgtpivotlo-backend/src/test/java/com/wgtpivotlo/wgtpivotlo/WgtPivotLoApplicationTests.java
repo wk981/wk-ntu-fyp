e460bf1c124f7b885ce8b5ed6fc8846496fc963f
wk981 <97287370+wk981@users.noreply.github.com>
2025-01-15 23:54:19 +0800

    Feat/cicd (#17)
    * feat: implement dockerfile for backend and updated docker-compose to env
    
    * refactor: refactor backend dockerfile. Now have to compile before docker build
    
    * feat: implement maven cicd workflow
    
    * feat: implement dockerfile and workflow for frontend
    
    * feat: update docker compose to offical image

--- wgtpivotlo-backend/src/test/java/com/wgtpivotlo/wgtpivotlo/WgtPivotLoApplicationTests.java
+++ wgtpivotlo-backend/src/test/java/com/wgtpivotlo/wgtpivotlo/WgtPivotLoApplicationTests.java
@@ -3,7 +3,12 @@ package com.wgtpivotlo.wgtpivotlo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=password",
+	"spring.datasource.driver-class-name=org.h2.Driver"
+})
 class WgtPivotLoApplicationTests {
 
 	@Test

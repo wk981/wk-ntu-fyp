beeb880a20a2c67d50fea44e3e8c7f048cb11934
wk981 <jamesim445@gmail.com>
2025-01-23 20:46:17 +0800

    fix: add environemnt to junit test

--- wgtpivotlo-backend/src/test/java/com/wgtpivotlo/wgtpivotlo/WgtPivotLoApplicationTests.java
+++ wgtpivotlo-backend/src/test/java/com/wgtpivotlo/wgtpivotlo/WgtPivotLoApplicationTests.java
@@ -7,7 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
     "spring.datasource.url=jdbc:h2:mem:testdb",
     "spring.datasource.username=sa",
     "spring.datasource.password=password",
-	"spring.datasource.driver-class-name=org.h2.Driver"
+	"spring.datasource.driver-class-name=org.h2.Driver",
+	"spring.cors.url=http://localhost:5173"
 })
 class WgtPivotLoApplicationTests {
 

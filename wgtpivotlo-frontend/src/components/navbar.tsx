55d1a74b1ee6f58f001b906a5e3a988e27fc390d
wk981 <jamesim445@gmail.com>
2025-01-25 01:23:06 +0800

    style: modify navbar for non log in and log in user

--- wgtpivotlo-frontend/src/components/navbar.tsx
+++ wgtpivotlo-frontend/src/components/navbar.tsx
@@ -40,28 +40,32 @@ export const Navbar = () => {
             <Link to={'/'}>
               <img src={HomeIcon}></img>{' '}
             </Link>
-            <div className="hidden md:block">
-              <div className="flex items-center space-x-4">
-                <BlueAnchorLink toPath="questionaire/upload" name="Questionnaire" />
-                <NavItemDropdown label="Explore" items={exploreMoreItems} />
-                <BlueAnchorLink toPath="resume" name="Resume" />
-              </div>
-            </div>
-            <div className="md:hidden">
-              <Button
-                onClick={() => setIsMenuOpen(!isMenuOpen)}
-                variant="ghost"
-                size="icon"
-                className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
-              >
-                <span className="sr-only">Open main menu</span>
-                {isMenuOpen ? (
-                  <X className="block h-6 w-6" aria-hidden="true" />
-                ) : (
-                  <Menu className="block h-6 w-6" aria-hidden="true" />
-                )}
-              </Button>
-            </div>
+            {isLoggedIn && (
+              <>
+                <div className="hidden md:block">
+                  <div className="flex items-center space-x-4">
+                    <BlueAnchorLink toPath="questionaire/upload" name="Questionnaire" />
+                    <NavItemDropdown label="Explore" items={exploreMoreItems} />
+                    <BlueAnchorLink toPath="resume" name="Resume" />
+                  </div>
+                </div>
+                <div className="md:hidden">
+                  <Button
+                    onClick={() => setIsMenuOpen(!isMenuOpen)}
+                    variant="ghost"
+                    size="icon"
+                    className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-blue-500"
+                  >
+                    <span className="sr-only">Open main menu</span>
+                    {isMenuOpen ? (
+                      <X className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Menu className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Button>
+                </div>
+              </>
+            )}
           </div>
 
           <div>

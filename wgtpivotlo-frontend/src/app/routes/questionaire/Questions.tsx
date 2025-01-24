45a4b2af1768f06ee6e80af32b66a5c67db6506d
wk981 <jamesim445@gmail.com>
2025-01-24 22:30:54 +0800

    feat: add resume link at upload page

--- wgtpivotlo-frontend/src/app/routes/questionaire/Questions.tsx
+++ wgtpivotlo-frontend/src/app/routes/questionaire/Questions.tsx
@@ -7,7 +7,7 @@ export const Questions = () => {
   const { resultPostMutation } = useQuestionaire();
   const { isPending } = resultPostMutation;
   return (
-    <div className="max-w-[1280px] mx-auto px-4 py-12 min-h-[calc(100vh-65px)]">
+    <div className="max-w-[1280px] mx-auto px-4 py-6 sm:py-12 min-h-[calc(100vh-65px)]">
       <Card className={`w-full max-w-md mx-auto py-9 ${isPending ? 'opacity-50 relative' : ''}`}>
         {isPending && (
           <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-50">

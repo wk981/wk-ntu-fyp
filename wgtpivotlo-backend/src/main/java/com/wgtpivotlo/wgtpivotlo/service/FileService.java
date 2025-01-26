954c2af75cc22e39bdca3f8727b47f329dd1ca92
wk981 <jamesim445@gmail.com>
2025-01-26 20:26:06 +0800

    feat: implement resume download feat

--- wgtpivotlo-backend/src/main/java/com/wgtpivotlo/wgtpivotlo/service/FileService.java
+++ wgtpivotlo-backend/src/main/java/com/wgtpivotlo/wgtpivotlo/service/FileService.java
@@ -17,6 +17,8 @@ import org.apache.pdfbox.text.PDFTextStripper;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.xwpf.usermodel.*;
+import org.apache.xmlbeans.XmlException;
+import org.openxmlformats.schemas.wordprocessingml.x2006.main.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -34,11 +36,32 @@ import java.util.stream.Collectors;
 public class FileService {
     private final SkillRepository skillRepository;
     private final UserSkillsRepository userSkillsRepository;
+    private final Map<String, Pattern> skillCategoryPatternMap;
+
     public static String output = "resume.docx";
+    public static String fontFamily = "Open Sans";
+    public static String cTAbstractNumBulletXML =
+            "<w:abstractNum xmlns:w=\"http://schemas.openxmlformats.org/wordprocessingml/2006/main\" w:abstractNumId=\"0\">"
+                    + "<w:multiLevelType w:val=\"hybridMultilevel\"/>"
+                    + "<w:lvl w:ilvl=\"0\"><w:start w:val=\"1\"/><w:numFmt w:val=\"bullet\"/><w:lvlText w:val=\"\"/><w:lvlJc w:val=\"left\"/><w:pPr><w:ind w:left=\"720\" w:hanging=\"360\"/></w:pPr><w:rPr><w:rFonts w:ascii=\"Symbol\" w:hAnsi=\"Symbol\" w:hint=\"default\"/></w:rPr></w:lvl>"
+                    + "<w:lvl w:ilvl=\"1\" w:tentative=\"1\"><w:start w:val=\"1\"/><w:numFmt w:val=\"bullet\"/><w:lvlText w:val=\"o\"/><w:lvlJc w:val=\"left\"/><w:pPr><w:ind w:left=\"1440\" w:hanging=\"360\"/></w:pPr><w:rPr><w:rFonts w:ascii=\"Courier New\" w:hAnsi=\"Courier New\" w:cs=\"Courier New\" w:hint=\"default\"/></w:rPr></w:lvl>"
+                    + "<w:lvl w:ilvl=\"2\" w:tentative=\"1\"><w:start w:val=\"1\"/><w:numFmt w:val=\"bullet\"/><w:lvlText w:val=\"\"/><w:lvlJc w:val=\"left\"/><w:pPr><w:ind w:left=\"2160\" w:hanging=\"360\"/></w:pPr><w:rPr><w:rFonts w:ascii=\"Wingdings\" w:hAnsi=\"Wingdings\" w:hint=\"default\"/></w:rPr></w:lvl>"
+                    + "</w:abstractNum>";
+
     @Autowired
     public FileService(SkillRepository skillRepository, UserSkillsRepository userSkillsRepository) {
         this.skillRepository = skillRepository;
         this.userSkillsRepository = userSkillsRepository;
+        skillCategoryPatternMap = new HashMap<>();
+        skillCategoryPatternMap.put("Frontend Framework", Pattern.compile("React|Angular", Pattern.CASE_INSENSITIVE));
+        skillCategoryPatternMap.put("Backend Framework", Pattern.compile("NodeJS|ExpressJS|FastAPI|Springboot", Pattern.CASE_INSENSITIVE));
+        skillCategoryPatternMap.put("Programming Language", Pattern.compile("Python|C\\+\\+|C#|JavaScript|Java|HTML|CSS|TypeScript|HTML and CSS|.*programming$", Pattern.CASE_INSENSITIVE));
+        skillCategoryPatternMap.put("DevOps", Pattern.compile("Docker|Kubernetes|Cloud API", Pattern.CASE_INSENSITIVE));
+        skillCategoryPatternMap.put("Databases", Pattern.compile(".*\\bsql\\b.*", Pattern.CASE_INSENSITIVE));
+        skillCategoryPatternMap.put("Version Control", Pattern.compile("Git|Github", Pattern.CASE_INSENSITIVE));
+        skillCategoryPatternMap.put("Communication", Pattern.compile("Leadership|Public Speaking|Communication", Pattern.CASE_INSENSITIVE));
+        skillCategoryPatternMap.put("Financial", Pattern.compile("Financial Analysis|Accounting|Budgeting", Pattern.CASE_INSENSITIVE));
+        skillCategoryPatternMap.put("Data", Pattern.compile(".*\\bdata\\b.*", Pattern.CASE_INSENSITIVE));
     }
 
     public String getOutput() {
@@ -150,44 +173,179 @@ public class FileService {
         Optional<List<UserSkills>> exitingUserSkillsList = userSkillsRepository.findByUserId(userId);
         exitingUserSkillsList.orElseThrow(() -> new ResourceNotFoundException("User does not have skills"));
 
-        XWPFDocument document = generateFile(exitingUserSkillsList.get());
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        document.write(baos);
-        document.close();
-
-        return baos.toByteArray();
+        return generateFile(exitingUserSkillsList.get());
 
     }
 
-    private XWPFDocument generateFile(List<UserSkills> userSkillsList) throws IOException {
+    private byte[] generateFile(List<UserSkills> userSkillsList) throws IOException, XmlException {
         XWPFDocument document = new XWPFDocument();
-        XWPFParagraph tmpParagraph = document.createParagraph();
-        XWPFRun tmpRun = tmpParagraph.createRun();
-        tmpRun.setText("LALALALAALALAAAA");
-        tmpRun.setFontSize(18);
-        return document;
-//        String fontFamily = "Open Sans";
-//        int defaultFontSize = 10;
-//        XWPFDocument document = new XWPFDocument();
-//
-//        XWPFParagraph title = document.createParagraph();
-//        title.setAlignment(ParagraphAlignment.CENTER);
-//
-//        log.info("Step 3a: Setting Title");
-//        XWPFRun titleRun = title.createRun();
-//        titleRun.setText("[name]");
-//        titleRun.setUnderline(UnderlinePatterns.SINGLE);
-//        titleRun.setBold(true);
-//        titleRun.setFontFamily(fontFamily);
-//        titleRun.setFontSize(defaultFontSize);
-//
-//        log.info("Step 3b: Setting Education");
-//
-//
-//        FileOutputStream out = new FileOutputStream(output);
-//        document.write(out);
-//        out.close();
-//        document.close();
-//        return document;
+        CTSectPr sectPr = document.getDocument().getBody().isSetSectPr()
+                ? document.getDocument().getBody().getSectPr()
+                : document.getDocument().getBody().addNewSectPr();
+
+        // Create or get page margin settings
+        CTPageMar pageMar = sectPr.isSetPgMar() ? sectPr.getPgMar() : sectPr.addNewPgMar();
+
+        // Set margins (values are in TWIPs: 1 inch = 1440 TWIPs, 0.5 inch = 720 TWIPs)
+        pageMar.setTop(720);    // Top margin: 0.5 inch
+        pageMar.setBottom(720); // Bottom margin: 0.5 inch
+        pageMar.setLeft(720);   // Left margin: 0.5 inch
+        pageMar.setRight(720);  // Right margin: 0.5 inch
+        pageMar.setGutter(0);   // Gutter: 0 inch
+
+        // Add a title
+        XWPFParagraph title = document.createParagraph();
+        title.setAlignment(ParagraphAlignment.CENTER);
+        XWPFRun titleRun = title.createRun();
+        titleRun.setText("[name]");
+        titleRun.setFontSize(10);
+        titleRun.setFontFamily(fontFamily);
+        titleRun.setBold(true);
+        title.setSpacingAfter(0); // No space after the title
+
+        // Add contact information
+        XWPFParagraph contactInfo = document.createParagraph();
+        contactInfo.setAlignment(ParagraphAlignment.CENTER);
+        XWPFRun contactRun = contactInfo.createRun();
+        contactRun.setText("Mobile Phone: / Email: / GitHub: /");
+        contactRun.setFontFamily(fontFamily);
+        contactRun.setFontSize(10);
+        contactInfo.setSpacingAfter(0);
+
+        // Add a section header
+        addSectionHeader(document, "EDUCATION");
+        document.createParagraph();
+
+        // Add technical skills with tight spacing
+        addSectionHeader(document, "TECHNICAL SKILLS");
+        Map<String, List<Skill>> res = new HashMap<>();
+        userSkillsList.stream().map(UserSkills::getSkill).forEach(skill -> processCategoryMap(res, skill)); // Categorize each skill));
+
+        addSkillsBullet(document,res);
+
+        // Add work experience with tight spacing
+        addSectionHeader(document, "WORK EXPERIENCE");
+
+        try (FileOutputStream out = new FileOutputStream(output)) {
+            document.write(out);
+            System.out.println("Document written to file: " + output);
+        }
+
+        byte[] byteArrayRes;
+        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            document.write(baos); // Write document content to memory
+            byteArrayRes = baos.toByteArray();
+            cleanup(document);
+        }
+
+        return byteArrayRes;
+
+    }
+
+    private static void addSectionHeader(XWPFDocument document, String text) {
+        XWPFParagraph header = document.createParagraph();
+        header.setBorderBottom(Borders.BASIC_THIN_LINES);
+        header.setSpacingAfter(0); // Tight spacing after section header
+        XWPFRun run = header.createRun();
+        run.setText(text);
+        run.setFontFamily(fontFamily);
+        run.setFontSize(9);
+        run.setBold(true);
+    }
+
+    private static void addBullet(XWPFDocument document, String text) {
+        XWPFParagraph paragraph = document.createParagraph();
+        paragraph.setIndentationLeft(360); // Indentation for bullet points
+        paragraph.setSpacingAfter(0); // Tight spacing after each bullet
+        paragraph.setSpacingBefore(0); // Tight spacing before each bullet
+        XWPFRun run = paragraph.createRun();
+        run.setFontFamily(fontFamily);
+        run.setText("• " + text);
+        run.setFontSize(9);
+    }
+
+    private static void addWorkExperience(XWPFDocument document, String title, String date, String[] points) {
+        XWPFParagraph experienceTitle = document.createParagraph();
+        XWPFRun titleRun = experienceTitle.createRun();
+        titleRun.setText(title);
+        titleRun.setBold(true);
+        experienceTitle.setSpacingAfter(0);
+
+        XWPFRun dateRun = experienceTitle.createRun();
+        dateRun.setFontFamily(fontFamily);
+        dateRun.setText(" " + date);
+        dateRun.setFontSize(9);
+        dateRun.setItalic(true);
+
+        for (String point : points) {
+            addBullet(document, point);
+        }
+    }
+
+    private void processCategoryMap(Map<String, List<Skill>> res, Skill skill){
+        int count = 0;
+        for (Map.Entry<String, Pattern> entry : skillCategoryPatternMap.entrySet()) {
+            String category = entry.getKey(); // Key: Category name
+            Pattern pattern = entry.getValue(); // Value: Pattern
+
+            if (pattern.matcher(skill.getName()).matches()) {
+                List<Skill> temp;
+                if(res.containsKey(category)){
+                    temp = res.get(category);
+                }
+                else{
+                    temp = new ArrayList<>();
+                }
+                temp.add(skill);
+                res.put(category, temp);
+                break;
+            }
+            count += 1;
+        }
+        if(count == skillCategoryPatternMap.size()){
+            List<Skill> temp;
+            if(res.containsKey("Others")){
+                temp = res.get("Others");
+            }
+            else{
+                temp = new ArrayList<>();
+            }
+            temp.add(skill);
+            res.put("Others", temp);
+        }
+        return;
+    }
+
+    private void addSkillsBullet(XWPFDocument document, Map<String, List<Skill>> res){
+        for (Map.Entry<String, List<Skill>> entry : res.entrySet()){
+            String category = entry.getKey();
+            List<Skill> skillList = entry.getValue();
+            String temp = category + ": " + skillList.stream()
+                    .map((skill) -> {
+                        return capitalizeFirstLetter(skill.getName());
+                    }) // Extract skill names
+                    .collect(Collectors.joining(", ")); // Join names with commas
+            addBullet(document, temp);
+        }
+    }
+
+    private static String capitalizeFirstLetter(String input) {
+        if (input == null || input.isEmpty()) {
+            return input; // Return input if null or empty
+        }
+        return input.substring(0, 1).toUpperCase() + input.substring(1).toLowerCase();
+    }
+
+    private void cleanup(XWPFDocument document) throws IOException {
+        // Delete the file
+        File file = new File(output);
+        if (file.delete()) {
+            log.info("File deleted successfully.");
+        } else {
+            log.info("File does not exist or file is deleted");
+        }
+
+        // Close the document
+        document.close();
     }
 }

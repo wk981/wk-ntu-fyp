a1bc146ec6e61c636c4327458eee69832a2cfc41
wk981 <97287370+wk981@users.noreply.github.com>
2025-02-28 16:27:39 +0800

    Feat/account settings and minor UI fix (#37)
    * fix: dashboard progression

--- wgtpivotlo-frontend/src/features/dashboard/components/SkillsToImproveProgressItem.tsx
+++ wgtpivotlo-frontend/src/features/dashboard/components/SkillsToImproveProgressItem.tsx
@@ -2,9 +2,10 @@ import { Progress } from '@/components/ui/progress';
 
 interface SkillsToImproveProgressItemProps {
   skillFlowList: string[];
+  inSkillSet: boolean;
 }
 
-export const SkillsToImproveProgressItem = ({ skillFlowList }: SkillsToImproveProgressItemProps) => {
+export const SkillsToImproveProgressItem = ({ skillFlowList, inSkillSet }: SkillsToImproveProgressItemProps) => {
   const proficiencyToNumber = (proficiency: string) => {
     switch (proficiency) {
       case 'Beginner':
@@ -22,7 +23,13 @@ export const SkillsToImproveProgressItem = ({ skillFlowList }: SkillsToImprovePr
   const targetLevel = proficiencyToNumber(skillFlowList[skillFlowList.length - 1]);
 
   // Normalize progress relative to the target
-  const progressValue = (startLevel / targetLevel) * 100;
+  const computeSkillGapProgress = (startLevel: number, targetLevel: number, inSkillSet: boolean) => {
+    if (startLevel === targetLevel && inSkillSet) {
+      return 100;
+    } else {
+      return (startLevel / targetLevel) * 100;
+    }
+  };
 
-  return <Progress value={progressValue} className="w-1/3" />;
+  return <Progress value={computeSkillGapProgress(startLevel, targetLevel, inSkillSet)} className="w-1/3" />;
 };
--- wgtpivotlo-frontend/src/features/dashboard/types/index.ts
+++ wgtpivotlo-frontend/src/features/dashboard/types/index.ts
@@ -1,16 +1,14 @@
-import { Career } from '@/features/questionaire/types';
 import { SkillDTO, skillsWithProfiency } from '@/features/skills/types';
 
 export interface DashboardDTOResponse {
-  username: string;
-  pic: string;
   careerProgression: number;
   userSkills: skillsWithProfiency[];
-  career: Career;
+  careerTitle: string;
   skillGap: SkillWIthCareerLevelFlowDTO[];
 }
 
 interface SkillWIthCareerLevelFlowDTO {
   skillDTO: SkillDTO;
   skillFlow: string[];
+  inSkillset: boolean;
 }
